ebe47cfada888e7a52b5b41b0243a78eb28918f7
Nicole Barkley <barkleylikecharles@icloud.com>
2022-06-28 23:32:15 -0400

    rebuild new portfolio

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import aboutImage from "../../assets/headshot.jpg";
+
+function About() {
+    return (
+        <section className="my-5">
+          <h1 id="about">Who am I?</h1>
+          <p id="about" > Welcome to my portfolio, and thank you for visiting!  My name is Nicole Barkley and a new full stack web developer and almost graduate.  </p>
+          <img src={aboutImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+        </section>
+              
+            );
+          }
+
+export default About;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+      <div>
+      <h1>Contact Me</h1>
+      <div>
+      <ul>
+            <li>706.296.5091</li>
+            <li>
+              <a href="mailto:barkleylikecharles@icloud.com">barkleylikecharles@icloud.com</a>
+            </li> 
+            <li>
+              <a href="https://github.com/barkleylikecharles">GitHub</a>
+            </li>
+          </ul>
+        </div>
+    </div>
+  );
+}
